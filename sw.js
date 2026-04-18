1fdf704f049c1eaa9ff08c2969dbfb100fe5164c
kumarvmcshubham <writetokumarshubham@gmail.com>
2026-04-18 22:48:47 +0530

    sw version pushed to 5
    sw version pushed to 5

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 // HarmonicaGuru — Service Worker
 
-const CACHE_NAME = 'harmonicaguru-v4';
+const CACHE_NAME = 'harmonicaguru-v5';
 const BASE = '/harmonicaguru';
 
 const SHELL_FILES = [
